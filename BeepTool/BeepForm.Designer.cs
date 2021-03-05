8b08bef918eee75f1f09c7c10ba79dbc99a938a2
李佳晉 <kuli7566@gmail.com>
2021-03-05 16:03:25 +0800

    加入專案檔案。

---
+++ BeepTool/BeepForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace BeepTool
+{
+    partial class BeepForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFrequency = new System.Windows.Forms.TextBox();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.txtDelay = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 165);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Frequency:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Duration:";
+            // 
+            // txtFrequency
+            // 
+            this.txtFrequency.Location = new System.Drawing.Point(90, 15);
+            this.txtFrequency.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtFrequency.Name = "txtFrequency";
+            this.txtFrequency.Size = new System.Drawing.Size(116, 23);
+            this.txtFrequency.TabIndex = 3;
+            this.txtFrequency.Text = "800";
+            this.toolTip1.SetToolTip(this.txtFrequency, "ccc");
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(90, 51);
+            this.txtDuration.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(116, 23);
+            this.txtDuration.TabIndex = 4;
+            this.txtDuration.Text = "200";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 5;
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(90, 87);
+            this.txtCount.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(116, 23);
+            this.txtCount.TabIndex = 8;
+            this.txtCount.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Count:";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 200;
+            this.toolTip1.ReshowDelay = 100;
+            this.toolTip1.ShowAlways = true;
+            // 
+            // txtDelay
+            // 
+            this.txtDelay.Location = new System.Drawing.Point(90, 123);
+            this.txtDelay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDelay.Name = "txtDelay";
+            this.txtDelay.Size = new System.Drawing.Size(116, 23);
+            this.txtDelay.TabIndex = 10;
+            this.txtDelay.Text = "200";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Delay:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtFrequency);
+            this.panel1.Controls.Add(this.txtDelay);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtCount);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtDuration);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 216);
+            this.panel1.TabIndex = 11;
+            // 
+            // BeepForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 216);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "BeepForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BeepForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFrequency;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtDelay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ BeepTool/BeepForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeepTool
+{
+    public partial class BeepForm : Form
+    {
+        public BeepForm()
+        {
+            InitializeComponent();
+
+            button1.Click += Button1_Click;
+
+            toolTip1.SetToolTip(label1, "The frequency of the beep, ranging from 37 to 32767 hertz.");
+            toolTip1.SetToolTip(label2, "The duration of the beep measured in milliseconds.");
+            toolTip1.SetToolTip(label4, "milliseconds.");      
+
+            txtFrequency.KeyPress += TextBox_KeyPress;
+            txtDuration.KeyPress += TextBox_KeyPress;
+            txtCount.KeyPress += TextBox_KeyPress;
+            txtDelay.KeyPress += TextBox_KeyPress;
+        }       
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int frequency = int.Parse(txtFrequency.Text);
+            int duration = int.Parse(txtDuration.Text);
+            int count = int.Parse(txtCount.Text);
+            int delay = int.Parse(txtDelay.Text);
+
+            if (frequency < 37 || frequency > 32767)
+            {
+                MessageBox.Show("The frequency of the beep, ranging from 37 to 32767 hertz", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            panel1.Enabled = false;
+
+            Task.Run(async () =>
+            {
+                do
+                {
+                    Console.Beep(frequency, duration);
+                    count--;
+
+                    if (count > 0)
+                        await Task.Delay(delay);
+
+                } while (count > 0);
+
+                Invoke((MethodInvoker)delegate
+                {
+                    panel1.Enabled = true;
+                });
+            });
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar < 48 | (int)e.KeyChar > 57)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
